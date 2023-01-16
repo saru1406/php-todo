f44103f2260f030ada8a22755e3af8a4bf09b719
saru1406 <y.s1406@ezweb.ne.jp>
2023-01-17 03:29:34 +0900

    [add]ログイン制限

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -7,11 +7,11 @@ use App\Models\Memo;
 
 class MemoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+ 
     public function index(Request $request)
     {
         $user = \Auth::user();
@@ -30,22 +30,11 @@ class MemoController extends Controller
         return view('memos.index', compact('user','memos','keyword'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('memos.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $memo=new Memo;
@@ -58,12 +47,6 @@ class MemoController extends Controller
         return redirect()->route('memos.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $user = \Auth::user();
@@ -73,12 +56,6 @@ class MemoController extends Controller
         return view('memos.show', compact('user','memo'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $user = \Auth::user();
@@ -88,13 +65,6 @@ class MemoController extends Controller
         return view('memos.edit', compact('user', 'memo'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $user = \Auth::user();
@@ -108,12 +78,6 @@ class MemoController extends Controller
         return redirect()->route('memos.show');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $user = \Auth::user();
